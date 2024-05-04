821109907ad62f39e6b756d49ebe9b34a9ebb4b5
Vadzim Matsiushonak <vadzimmatsiushonak@gmail.com>
2024-05-05 02:37:39 +0300

    Add application startup listener

--- src/main/java/by/vadzimmatsiushonak/ttlservice/TtlServiceApplication.java
+++ src/main/java/by/vadzimmatsiushonak/ttlservice/TtlServiceApplication.java
@@ -1,37 +1,15 @@
 package by.vadzimmatsiushonak.ttlservice;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.cache.Cache;
-import org.springframework.cache.Cache.ValueWrapper;
-import org.springframework.context.event.EventListener;
+import org.springframework.cache.annotation.EnableCaching;
 
-@Slf4j
+@EnableCaching
 @SpringBootApplication
 public class TtlServiceApplication {
 
-    @Autowired
-    private Cache defaultCache;
-
     public static void main(String[] args) {
         SpringApplication.run(TtlServiceApplication.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void startup() throws InterruptedException {
-        log.info("Application started");
-        defaultCache.put("key", "value");
-        log.info("put 'key':'value' in defaultCache");
-        ValueWrapper value = defaultCache.get("key");
-        log.info("Get 'key':{}", value);
-        SECONDS.sleep(5l);
-        value = defaultCache.get("key");
-        log.info("Get 'key':{}", value);
     }
 
 }
---
+++ src/main/java/by/vadzimmatsiushonak/ttlservice/listener/ApplicationListener.java
@@ -0,0 +1,33 @@
+package by.vadzimmatsiushonak.ttlservice.listener;
+
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.Cache;
+import org.springframework.cache.Cache.ValueWrapper;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ApplicationListener {
+
+    private final Cache defaultCache;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void startup() throws InterruptedException {
+        log.info("Application started");
+        defaultCache.put("key", "value");
+        log.info("put 'key':'value' in defaultCache");
+        ValueWrapper value = defaultCache.get("key");
+        log.info("Get 'key':{}", value);
+        SECONDS.sleep(5L);
+        value = defaultCache.get("key");
+        log.info("Get 'key':{}", value);
+    }
+
+}
